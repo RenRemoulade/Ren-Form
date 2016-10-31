1ed442934224603a583506aca76c1b9d16ef8b0d
Simon Sinding <sinding2000@gmail.com>
2016-10-31 23:05:30 +0100

    Added vue, made tests, create auth

--- Ren Form/routes/web.php
+++ Ren Form/routes/web.php
@@ -14,3 +14,32 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('customer/{id}', function($id){
+  $customer = App\Customer::find($id);
+  foreach($customer->orders as $order){
+    echo $order->name . '<br>';
+  }
+  echo $customer;
+});
+
+Route::get('customer_name/{name}', 'CustomerController@customer');
+
+Route::get('orders', function(){
+  $orders = App\Order::all();
+  foreach($orders as $order){
+    echo $order->customer->name . " bought " . $order->name . "<br>";
+  }
+});
+
+Route::get('mypage', function(){
+  $data = array('var1' => 'hey'
+  , 'var2' => 'hotdog'
+  , 'var3' => 'moar'
+  , 'orders' => App\Order::all());
+  return view('mypage', $data);
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
